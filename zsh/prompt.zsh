9cfe271b589086ac1764d22d20656fb180db6576
Christian Wemstad <wemstad@kth.se>
2013-02-09 15:38:16 +0100

    Removed unsed files

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -56,24 +56,24 @@ rb_prompt(){
   fi
 }
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if (( $+commands[todo.sh] ))
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
+# # This keeps the number of todos always available the right hand side of my
+# # command line. I filter it to only count those tagged as "+next", so it's more
+# # of a motivation to clear out the list.
+# todo(){
+#   if (( $+commands[todo.sh] ))
+#   then
+#     num=$(echo $(todo.sh ls +next | wc -l))
+#     let todos=num-2
+#     if [ $todos != 0 ]
+#     then
+#       echo "$todos"
+#     else
+#       echo ""
+#     fi
+#   else
+#     echo ""
+#   fi
+# }
 
 directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
