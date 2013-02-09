e1ef033b9fbf7285e1d9f361f3fa98d97351880f
Christian Wemstad <wemstad@kth.se>
2013-02-09 14:23:49 +0100

    Changed so completion uses zsh instaid of bash

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,7 +1,8 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+# Changed to use zsh autocomplietion instaid.
+completion=/usr/local/share/zsh/site-functions
 
 if test -f $completion
 then
