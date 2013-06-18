e7beb0a74292f636eca77f1c0055d467100eab8c
Christian Wemstad <wemstad@kth.se>
2013-06-18 07:48:24 +0200

    Some small changes to make boxen work better

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/sbin"
+export PATH="/opt/boxen/homebrew/bin:/opt/boxen/hombrew/sbin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/sbin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
