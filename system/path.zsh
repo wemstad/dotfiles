5da4930e3f41346c9b3be05c5c1c6722d764fe2d
wemstad <wemstad@kth.se>
2013-11-23 23:29:33 +0100

    Adding texlive to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/opt/boxen/homebrew/bin:/opt/boxen/hombrew/sbin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/sbin"
+export PATH="$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/texlive/2013/bin/x86_64-darwin:$PATH:/sbin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
