a680af88c47ff9ad01c933dbcfef7bf2996d085f
Christian Wemstad <wemstad@kth.se>
2014-09-26 10:49:50 +0200

    Fix for latex 2014

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/texlive/2013/bin/x86_64-darwin:$PATH:/sbin:/usr/texbin:"
+export PATH="$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/texlive/2014/bin/x86_64-darwin:$PATH:/sbin:/usr/texbin:"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
